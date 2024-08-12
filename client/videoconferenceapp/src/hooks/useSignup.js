8083ed40c74ca399d6a6fe9fbf384a8b5ed5e007
hritikmohan <hhritikmohan@gmail.com>
2024-08-12 18:17:45 +0530

    Edit

--- client/videoconferenceapp/src/hooks/useSignup.js
+++ client/videoconferenceapp/src/hooks/useSignup.js
@@ -17,9 +17,6 @@ const useSignup = () => {
             setLoading(false);
             const res = await fetch('http://localhost:3000/api/auth/signup', {
                 method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
                 body: JSON.stringify(values),
             });
 
